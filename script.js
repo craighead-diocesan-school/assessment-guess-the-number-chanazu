2b67ea57e552f65d663fe08ee6cf3c0679b57f12
Azu Chan <Kogaaz68@craighead.school.nz>
2024-08-21 22:52:58 +0000

    a

---
+++ script.js
@@ -0,0 +1,14 @@
+function numberGame(){
+alert("Let's Guess the Number game")
+
+let name = prompt('What is your name?')
+let answer = prompt ('Hi,' + name + "I'm thinking of a number between 1 and 20 inclusive.")
+
+let userAttempt = prompt("What is your guess?")
+let computerNumber = Math.floor(Math.random()*(20 - 1) + 1)
+
+
+"Nope!!😶"
+"Well done!👀"
+
+}
